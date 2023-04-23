7db046e876550b13571a9962af49c62da9e69341
Yash Chauhan <yashchauhan858@gmail.com>
2023-04-23 14:09:07 +0530

    explaination added

--- App.js
+++ App.js
@@ -3,13 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider, useSelector } from "react-redux";
 import { store } from "./Store/store";
 import HomeScreen from "./screens/HomeScreen";
+/**
+ * The SafeAreaProvider component is part of the react-native-safe-area-context
+ * package and provides a way to automatically handle safe area insets in
+ * your React Native application.
+ * Safe area insets are the area on the screen that are not covered by notches,
+ * device rounded corners or the home indicator on newer iPhones. The
+ * SafeAreaProvider component wraps your entire application and provides
+ * information about the safe area insets to its child components.
+ * This information can be used to adjust the layout of your application so
+ * that content is not obscured by these areas.
+ */
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
+      <SafeAreaProvider>
         <HomeScreen />
-      </View>
+      </SafeAreaProvider>
     </Provider>
   );
 }
