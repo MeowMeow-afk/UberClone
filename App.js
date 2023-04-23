9f8d5345255e1258b04cbe30f37aba28b4004e6e
Yash Chauhan <yashchauhan858@gmail.com>
2023-04-23 12:26:29 +0530

    first tester screen added

--- App.js
+++ App.js
@@ -2,27 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider, useSelector } from "react-redux";
 import { store } from "./Store/store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Open up  App.js to start working on your app!</Text>
-        <ReduxData />
-        <StatusBar style="auto" />
+        <HomeScreen />
       </View>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
 
 const ReduxData = () => {
   const data = useSelector((state) => state.store);
