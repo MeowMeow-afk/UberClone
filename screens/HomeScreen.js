1230f8d627cc638ba99423f42fabcbd32c276479
Yash Chauhan <yashchauhan858@gmail.com>
2023-04-23 13:06:23 +0530

    image component added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <Text style={tw`text-red-500 p-10`}>HomeScreen</Text>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={require("../assets/uber_logo.png")}
+        />
+      </View>
     </SafeAreaView>
   );
 };
